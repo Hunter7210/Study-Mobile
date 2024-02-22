aacee19b0f7f9ea97dc91ca6a3a09478935b62f5
Hunter7210 <heitorrmsilva@gmail.com>
2024-02-22 14:58:22 -0300

    Jogo Advinha Dart

--- jogo_adivinha/lib/main.dart
+++ jogo_adivinha/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'dart:math';
 
@@ -21,38 +23,58 @@ class Calculadora extends StatefulWidget {
 
 class _CalculadoraState extends State<Calculadora> {
   //Criação de um objeto
-  TextEditingController _controllerNumero1 = TextEditingController();
 
+  TextEditingController _controllerNumero1 = TextEditingController();
   String _resultado = ''; //String em "" ou ''
 
-  void _calcular(String operacao) {
-    Random random = new Random();
-    int gerad = random.nextInt(100);
+  //Chamando o Random
+  int gerad = Random().nextInt(99) + 1;
+  int contad = 0;
+
+  void _calcular() {
     double numero1 = double.tryParse(_controllerNumero1.text) ??
         0.0; //Pegando o texto do controllerNumero1 e passa para double
     double? resultado;
-    int contad = 0;
-
     bool venceu = true;
 
+//Métodos
+
+    //Criando uma classe para conter todas as verificações
     setState(() {
-      while (venceu == true) {
+      while (venceu) {
+        contad++;
         if (numero1 == gerad) {
           //Você ganhou
-          _resultado = "Você ganhou; Você acertou em $contad tentativas: ";
-          venceu = false;
+          _resultado = "Você ganhou; Você acertou em: $contad tentativas";
+          venceu = true;
+          print("Acertou");
         } else if (numero1 < gerad) {
           //Seu valor esta baixo
           _resultado = "Seu valor esta baixo";
+          print("Ta baixo");
         } else if (numero1 > gerad) {
           //Diminua o valor
           _resultado = "Seu valor esta muito alto";
+          print("Ta alto");
         }
+        venceu = false;
+        print(gerad);
       }
     });
   }
 
-  @override
+  void _resetar() {
+    gerad = Random().nextInt(9) + 1;
+    contad = 0;
+
+    _resultado = 'Você resetou com sucesso';
+    setState(() {
+      _controllerNumero1.text = "";
+    });
+  }
+
+  //Método responsável pela parte visual
+  @override //Sobreescrever o metodo,
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -71,13 +93,13 @@ class _CalculadoraState extends State<Calculadora> {
             ),
             SizedBox(height: 16.0),
             ElevatedButton(
-              onPressed: () => _calcular(
-                  'Somar'), //onPressed = evento; Ao pressionar o botão ele atribuí a calcular o valor "Soma"
+              onPressed: () =>
+                  _calcular(), //onPressed = evento; Ao pressionar o botão ele atribuí a calcular o valor "Soma"
               child: Text('Testar'),
             ),
             SizedBox(height: 10.0),
             ElevatedButton(
-              onPressed: () => _calcular('Sub'),
+              onPressed: () => _resetar(),
               child: Text('Resetar'),
             ),
             SizedBox(height: 16.0),
