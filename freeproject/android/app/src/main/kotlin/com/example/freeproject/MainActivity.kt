0cbe3d66af8bd9c1e8155cca1e58e5f17e7449df
Hunter7210 <heitorrmsilva@gmail.com>
2024-02-28 23:24:47 -0300

    28/02/2024

---
+++ freeproject/android/app/src/main/kotlin/com/example/freeproject/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.freeproject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ freeproject/lib/src/widgets/display.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Display extends StatefulWidget {
+  final String value;
+  const Display({super.key, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Text(
+          value,
+          style: Theme.of(context).textTheme.displayLarge,
+        ),
+      ),
+    );
+  }
+}
