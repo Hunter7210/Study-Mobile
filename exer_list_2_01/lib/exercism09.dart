0dcd5d4b672b89608d0848154466dac9d5cc4e8c
Hunter7210 <heitorrmsilva@gmail.com>
2024-03-18 16:18:30 -0300

    upgrade_list_2_exerc

---
+++ exer_list_2_01/lib/exercism09.dart
@@ -0,0 +1,95 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Exercism09 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Personalização de Botões"),
+        backgroundColor: Colors.cyan,
+      ),
+      body: Column(
+        children: [
+          Text("ElevateButton"),
+          Row(children: [
+            ElevatedButton(
+              onPressed: () {},
+              child: Icon(Icons.construction_outlined),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.amber,
+                foregroundColor: Colors.black,
+                textStyle: TextStyle(fontSize: 20.0),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("Enviar"),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.amber,
+                foregroundColor: Colors.black,
+                textStyle: TextStyle(fontSize: 20.0),
+              ),
+            )
+          ]),
+          Text("TextButton"),
+          Row(
+            children: [
+              TextButton(
+                onPressed: () {},
+                child: Text("Clique aqui!"),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.amber,
+                  foregroundColor: Colors.black,
+                  textStyle: TextStyle(fontSize: 20.0),
+                ),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: Text("Teste"),
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.amber,
+                  foregroundColor: Colors.black,
+                  textStyle: TextStyle(fontSize: 20.0),
+                ),
+              )
+            ],
+          ),
+          Text("OutlinedButton"),
+          Row(
+            children: [
+              OutlinedButton(
+                onPressed: () {},
+                child: Text("Pressione"),
+                style: OutlinedButton.styleFrom(
+                  backgroundColor: Colors.amber,
+                  foregroundColor: Colors.black,
+                  textStyle: TextStyle(fontSize: 20.0),
+                ),
+              ),
+              OutlinedButton(
+                onPressed: () {},
+                child: Text("Botão1"),
+                style: OutlinedButton.styleFrom(
+                  // Cor do texto
+                  textStyle: TextStyle(fontSize: 20), // Estilo do texto
+                  padding: EdgeInsets.symmetric(
+                      horizontal: 20, vertical: 10), // Espaçamento interno
+                  shape: RoundedRectangleBorder(
+                    borderRadius:
+                        BorderRadius.circular(20), // Borda arredondada
+                    side: BorderSide(
+                        color: Colors.blue), // Borda ao redor do botão),
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
--- exer_list_2_01/lib/main.dart
+++ exer_list_2_01/lib/main.dart
@@ -6,6 +6,7 @@ import 'package:exer_list_2_01/exercism05.dart';
 import 'package:exer_list_2_01/exercism06.dart';
 import 'package:exer_list_2_01/exercism07.dart';
 import 'package:exer_list_2_01/exercism08.dart';
+import 'package:exer_list_2_01/exercism09.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -20,7 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      home: Exercism08(),
+      home: Exercism09(),
     );
   }
 }
