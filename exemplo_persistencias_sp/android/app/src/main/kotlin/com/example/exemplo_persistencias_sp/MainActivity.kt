1763e45c7ec8f00946f516cd583866c1b4b60a67
Hunter7210 <heitorrmsilva@gmail.com>
2024-03-19 16:56:57 -0300

    19/03/2024

---
+++ exemplo_persistencias_sp/android/app/src/main/kotlin/com/example/exemplo_persistencias_sp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.exemplo_persistencias_sp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- exer_list_2_01/lib/exercism10.dart
+++ exer_list_2_01/lib/exercism10.dart
@@ -1,93 +1,53 @@
 import 'package:flutter/material.dart';
 
-class Exercism10 extends StatelessWidget {
+class Exercism10 extends StatefulWidget {
+  const Exercism10({Key? key}) : super(key: key);
+
+  @override
+  _Exercism10State createState() => _Exercism10State();
+}
+
+class _Exercism10State extends State<Exercism10> with TickerProviderStateMixin {
+  late AnimationController controller;
+
   @override
   void initState() {
+    super.initState();
     controller = AnimationController(
       vsync: this,
-      duration: const Duration(seconds: 4),
+      duration: const Duration(seconds: 1),
     )..addListener(() {
         setState(() {});
       });
-    controller.repeat(reverse: true);
-    super.initState();
+    controller.repeat(reverse: false);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Carregamento"),
-        backgroundColor: Colors.indigoAccent,
-      ),
-      backgroundColor: Colors.blue[50],
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Text("Carregamento em Circulo"),
-          CircularProgressIndicator(
-            valueColor: AlwaysStoppedAnimation<Color>(Colors.greenAccent),
-            value: 10,
-            strokeWidth: 6,
-          )
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Circular progress indicator with a fixed color',
+              style: Theme.of(context).textTheme.bodyText1,
+            ),
+            CircularProgressIndicator(
+              value: controller.value,
+              semanticsLabel: 'Circular progress indicator',
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
-//   const Exercism10({Key? key}) : super(key: key);
-
-//   @override
-//   State<Exercism10> createState() => _ProgressIndicatorExampleState();
-// }
-
-// class _ProgressIndicatorExampleState extends State<Exercism10>
-//     with TickerProviderStateMixin {
-//   late AnimationController controller;
-
-//   @override
-//   void initState() {
-//     controller = AnimationController(
-//       vsync: this,
-//       duration: const Duration(seconds: 1),
-//     )..addListener(() {
-//         setState(() {});
-//       });
-//     controller.repeat(reverse: false);
-//     super.initState();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Padding(
-//         padding: const EdgeInsets.all(20.0),
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//           crossAxisAlignment: CrossAxisAlignment.center,
-//           children: <Widget>[
-//             Text(
-//               'Circular progress indicator with a fixed color',
-//               style: Theme.of(context).textTheme.bodyLarge,
-//             ),
-//             CircularProgressIndicator(
-//               value: controller.value,
-//               semanticsLabel: 'Circular progress indicator',
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-//}
