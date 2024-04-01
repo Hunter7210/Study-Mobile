0199a11818d70f6fd2c92cdd1a332b2ba60b5cb6
Hunter7210 <heitorrmsilva@gmail.com>
2024-04-01 15:27:24 -0300

    01/04/2024

--- exemplo_persistencia_sqllite/lib/DataBaseController.dart
+++ exemplo_persistencia_sqllite/lib/DataBaseController.dart
@@ -1,3 +1,6 @@
+import 'dart:html';
+
+import 'package:exemplo_persistencia_sqllite/Model.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
@@ -6,15 +9,96 @@ class DatabaseHelper {
   static const String DATABASE_NAME = 'contacts.db';
   static const String TABLE_NAME = 'contacts';
   static const String CREATE_CONTACTS_TABLE_SCRIPT =
-      'CREATE TABLE contacts(id INTEGER PRIMARY KEY, name TEXT, email TEXT, phone TEXT, addressLine1 TEXT)';
+      'CREATE TABLE contacts(id SERIAL PRIMARY KEY, name TEXT, email TEXT, phone TEXT, addressLine1 TEXT)';
 
+  //Future -significa que ela vai ser uma ação executa ao longo da aplicação
   Future<Database> _getDatabase() async {
+    // DATABASE É CLASSE DO SQL LITE
     return openDatabase(
+      //Usar o openDatabase para abrir a conexão, sempre realizar isso antes de qualquer ação no banco.
       join(await getDatabasesPath(), DATABASE_NAME),
       onCreate: (db, version) {
         return db.execute(CREATE_CONTACTS_TABLE_SCRIPT);
       },
       version: 1,
     );
-  } //Future -significa que ela vai ser uma ação executa ao longo da aplicação
+  }
+
+  // Método para criar um novo contato no banco de dados
+  Future<void> create(ContactModel model) async {
+    try {
+      final Database db = await _getDatabase();
+      await db.insert(
+          TABLE_NAME, model.toMap()); // Insere o contato no banco de dados
+    } catch (ex) {
+      print(ex);
+      return;
+    }
+  }
+
+  // Método para obter todos os contatos do banco de dados
+  Future<List<ContactModel>> getContacts() async {
+    try {
+      final Database db = await _getDatabase();
+      final List<Map<String, dynamic>> maps =
+          await db.query(TABLE_NAME); // Consulta todos os contatos na tabela
+
+      return List.generate(
+        maps.length,
+        (i) {
+          return ContactModel.fromMap(maps[
+              i]); // Converte os resultados da consulta para objetos ContactModel
+        },
+      );
+    } catch (ex) {
+      print(ex);
+      return [];
+    }
+  }
+
+  // Método para atualizar um contato no banco de dados
+  Future<void> update(ContactModel model) async {
+    try {
+      final Database db = await _getDatabase();
+      await db.update(
+        TABLE_NAME,
+        model.toMap(),
+        where: "id = ?", // Condição para atualizar o contato com base no ID
+        whereArgs: [model.id],
+      );
+    } catch (ex) {
+      print(ex);
+      return;
+    }
+  }
+
+  // Método para excluir um contato do banco de dados
+  Future<void> delete(int id) async {
+    try {
+      final Database db = await _getDatabase();
+      await db.delete(
+        TABLE_NAME,
+        where: "id = ?", // Condição para excluir o contato com base no ID
+        whereArgs: [id],
+      );
+    } catch (ex) {
+      print(ex);
+      return;
+    }
+  }
+
+  // Método para excluir um contato do banco de dados
+  Future<void> inserir(ContactModel model) async {
+    try {
+      final Database db = await _getDatabase();
+
+      await db.insert(
+        TABLE_NAME,
+        model.toMap(),
+      );
+    } catch (ex) {
+      print(ex);
+      return;
+    }
+  }
 }
--- exemplo_persistencia_sqllite/lib/View.dart
+++ exemplo_persistencia_sqllite/lib/View.dart
@@ -1,10 +1,123 @@
+import 'dart:html';
+
+import 'package:exemplo_persistencia_sqllite/DataBaseController.dart';
+import 'package:exemplo_persistencia_sqllite/Model.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final dbHelper = DatabaseHelper();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('SQLite Demo'),
+      ),
+      body: FutureBuilder<List<ContactModel>>(
+        future: dbHelper
+            .getContacts(), // Obtém a lista de contatos do banco de dados
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+                child:
+                    CircularProgressIndicator()); // Exibe um indicador de carregamento enquanto os dados estão sendo carregados
+          } else if (snapshot.hasError) {
+            return Center(
+                child: Text(
+                    'Error: ${snapshot.error}')); // Exibe uma mensagem de erro se ocorrer um erro ao carregar os dados
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(
+                child: Text(
+                    'No contacts found.')); // Exibe uma mensagem se não houver contatos encontrados
+          } else {
+            return ListView.builder(
+              itemCount: snapshot.data?.length,
+              itemBuilder: (context, index) {
+                final contact = snapshot.data?[index];
+                return ListTile(
+                  title: Text(contact!.name),
+                  subtitle: Text(contact.email),
+                  onTap: () {
+                    // Implement onTap functionality
+                  },
+                );
+              },
+            ); // Exibe a lista de contatos
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _alertAtuali(
+            context,
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  void _alertAtuali(
+    BuildContext context,
+  ) async {
+    TextEditingController _idcontroller = TextEditingController();
+    TextEditingController _nomecontroller = TextEditingController();
+    TextEditingController _emailcontroller = TextEditingController();
+    TextEditingController _phonecontroller = TextEditingController();
+    TextEditingController _addressLine1controller = TextEditingController();
+
+    return showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Preencha os campos"),
+          content: Column(
+            children: [
+              TextField(
+                controller: _nomecontroller,
+                decoration: InputDecoration(hintText: "Digite o nome:"),
+              ),
+              TextField(
+                controller: _emailcontroller,
+                decoration: InputDecoration(hintText: "Digite o nome:"),
+              ),
+              TextField(
+                controller: _phonecontroller,
+                decoration: InputDecoration(hintText: "Digite o nome:"),
+              ),
+              TextField(
+                controller: _addressLine1controller,
+                decoration: InputDecoration(hintText: "Digite o nome:"),
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                if (context.mounted) {
+                  Navigator.of(context).pop();
+                }
+              },
+              child: Text('Atualizar'),
+            ),
+            TextButton(
+              onPressed: () {
+                if (context.mounted) {
+                  Navigator.of(context).pop();
+                }
+              },
+              child: Text('Fechar'),
+            ),
+          ],
+        );
+      },
+    );
   }
 }
