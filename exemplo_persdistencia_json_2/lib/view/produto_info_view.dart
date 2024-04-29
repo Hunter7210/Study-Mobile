78ac6e25090ec36d66745a22555a65049a4fc260
Hunter7210 <heitorrmsilva@gmail.com>
2024-04-29 16:52:17 -0300

    29/04/2024

--- exemplo_persdistencia_json_2/lib/view/produto_info_view.dart
+++ exemplo_persdistencia_json_2/lib/view/produto_info_view.dart
@@ -19,10 +19,7 @@ class ProdutoInfoPage extends StatelessWidget {
             // ignore: prefer_interpolation_to_compose_strings
             Text('Preco: ${produto.preco}'),
             Text(produto.categoria),
-            Image.asset(
-              produto.url,
-              fit: Border.all(color: Colors.amber, style: BorderStyle.solid),
-            )
+            Image.asset(produto.url)
           ],
         ),
       ),
